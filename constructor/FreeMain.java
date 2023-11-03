36ccfc6b7993b29b6ba1ed85065eed617853fd98
Mufeedasf <149234011+Mufeedasf@users.noreply.github.com>
2023-11-03 11:37:51 +0530

    Update FreeMain.java

--- constructor/FreeMain.java
+++ constructor/FreeMain.java
@@ -3,10 +3,10 @@ class FreeMain
 public static void main(String[] args)
 {
 	System.out.println("Main Running");
-	Free free=new Free();
+	Free free=new Free("Keychain","offer",true);
 	free.display();
 	
-	Free free1=new Free("Bag");
+	Free free1=new Free("Bag","referal code",false);
 	free1.display();
 	
 	Free free2=new Free("Phone","Discount",true);
@@ -15,11 +15,29 @@ public static void main(String[] args)
 	Free free3=new Free();
 	free3.display();
 	
-	Free free4=new Free("Book","offer");
+	Free free4=new Free("Book","offer",true);
 	free4.display();
 	
-	Free free5=new Free();
+	Free free5=new Free("Tissue","Free",false);
 	free5.display();
+	System.out.println("Main Running");
+	Free free=new Free("Keychain","offer",true);
+	free.display();
+	
+	Free free6=new Free("Dree","referal code",false);
+	free6.display();
+	
+	Free free7=new Free("Pin","Discount",true);
+	free2.display();
+	
+	Free free8=new Free("Box","refrence",true);
+	free8.display();
+	
+	Free free9=new Free("Book","offer",true);
+	free9.display();
+	
+	Free free10=new Free("Tissue","Free",false);
+	free10.display();
 	
 	System.out.println("Main Ending");
 }
