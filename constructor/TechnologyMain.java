67672dc5089d893c41ad597ce3337425f4239a62
Mufeedasf <149234011+Mufeedasf@users.noreply.github.com>
2023-11-03 14:15:11 +0530

    Update TechnologyMain.java

--- constructor/TechnologyMain.java
+++ constructor/TechnologyMain.java
@@ -3,27 +3,63 @@ class TechnologyMain
 public static void main(String[] args)
 {
 	System.out.println("Main Running");
-	Technology technology=new Technology("XML","yse","normal",4555,);
+	Technology technology=new Technology("XML","yse","normal",4555,45,"C",true,"small",
+					    12,false,"Present",true);
 	technology.display();
 	
-	Technology technology1=new Technology("Full-stack");
+	Technology technology1=new Technology("Full-stack","No","normal",1999,45,"C",true,"small",
+					    10,false,"Present",true);
 	technology1.display();
 	
-	Technology technology2=new Technology("Training","Available");
+	Technology technology2=new Technology("Training","Available","normal",5677,45,"C",true,"small",
+					    2,false,"Present",true);
 	technology2.display();
 	
-	Technology technology3=new Technology("Small","Present",false);
+	Technology technology3=new Technology("Small","Present","ormal",4555,45,"C",true,"small",
+					    12,false,"Present",true);
 	technology3.display();
 	
-	Technology technology4=new Technology("Web","Standard",true,15);
+	Technology technology4=new Technology("Web","Standard","normal",6755,15,"C++",true,"small",
+					    12,false,"Present",true);
 	technology4.display();
 	
-	Technology technology5=new Technology(true,34,false,25);
+	Technology technology5=new Technology("XML","no","normal",9055,45,"C",true,"small",
+					    12,false,"Present",true);
 	technology5.display();
 	
 	
-	Technology technology6=new Technology("Advance",3999.9,"Half","Python");
+	Technology technology6=new Technology("XML","yse","normal",3555,7,"C",true,"small",
+					    12,false,"Present",true);
 	technology6.display();
+	
+	Technology technology7=new Technology("XML","yse","normal",4555,45,"C",true,"small",
+					    12,false,"Present",true);
+	technology7.display();
+	
+	Technology technology8=new Technology("Full-stack","No","normal",1999,45,"C",true,"small",
+					    10,false,"Present",true);
+	technology8.display();
+	
+	Technology technology9=new Technology("Training","Available","normal",5677,45,"C",true,"small",
+					    2,false,"Present",true);
+	technology9.display();
+	
+	Technology technology10=new Technology("Small","Present","ormal",4555,45,"C",true,"small",
+					    12,false,"Present",true);
+	technology10.display();
+	
+	Technology technology11=new Technology("Web","Standard","normal",6755,15,"C++",true,"small",
+					    12,false,"Present",true);
+	technology11.display();
+	
+	Technology technology12=new Technology("XML","no","normal",9055,45,"C",true,"small",
+					    12,false,"Present",true);
+	technology12.display();
+	
+	
+	Technology technology13=new Technology("XML","yse","normal",3555,7,"C",true,"small",
+					    12,false,"Present",true);
+	technology13.display();
 	
 	
 	System.out.println("Main Ending");
