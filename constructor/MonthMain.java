27a3965cb3e828f1b4094e81e4a36c9bb1f9e74b
Mufeedasf <149234011+Mufeedasf@users.noreply.github.com>
2023-11-03 11:48:14 +0530

    Update MonthMain.java

--- constructor/MonthMain.java
+++ constructor/MonthMain.java
@@ -3,27 +3,46 @@ class MonthMain
 public static void main(String[] args)
 {
 	System.out.println("Main Running");
-	Month month=new Month();
+	Month month=new Month("Monday",30,3,5);
 	month.display();
 	
-	Month month1=new Month("Thursday");
+	Month month1=new Month("Thursday",28,4,5);
 	month1.display();
 	
-	Month month2=new Month("Friday",31);
+	Month month2=new Month("Friday",31,6.4);
 	month2.display();
 	
-	Month month3=new Month("Tuesday",30,4);
+	Month month3=new Month("Tuesday",30,4,2);
 	month3.display();
 	
 	Month month4=new Month("Wednesday",28,5,3);
 	month4.display();
 	
-	Month month5=new Month("Saturday",30);
+	Month month5=new Month("Saturday",30,4,6);
 	month5.display();
 	
 	
-	Month month6=new Month("Sunday",20);
+	Month month6=new Month("Monday",30,3,5);
 	month6.display();
+	
+	Month month7=new Month("Thursday",28,4,5);
+	month7.display();
+	
+	Month month8=new Month("Friday",31,6.4);
+	month8.display();
+	
+	Month month9=new Month("Tuesday",30,4,2);
+	month9.display();
+	
+	Month month10=new Month("Wednesday",28,5,3);
+	month10.display();
+	
+	Month month11=new Month("Saturday",30,4,6);
+	month11.display();
+	
+	
+	Month month12=new Month("Sunday",20);
+	month12.display();
 	
 	
 	System.out.println("Main Ending");
