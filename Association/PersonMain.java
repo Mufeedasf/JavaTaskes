f25ec6d36d4e29476524468c61c3f6d019bc42b2
Mufeedasf <mufidasf@gmail.com>
2023-11-08 21:21:41 +0530

    Task added

--- Association/PersonMain.java
+++ Association/PersonMain.java
@@ -7,17 +7,18 @@ public static void main(String args[])
 	Email email=new Email(domain,25,"Carry","Minati");
 	Email email2=new Email(domain,35,"Kavana","Nager");
 	Email email3=new Email(domain,45,"Archana","G R");
-	
-	
-	Person person=new Person("Ajey",email);
-	person.show();
-	
 	String[] email1=new String[4];
 	email1[0]="ABC@gmail.com";
 	email1[1]="PQR@gmail.com";
 	email1[2]="UVW@gmail.com";
 	
+	
+	Person person=new Person("Ajey",email);
+	
 	person.setEmail(email1);
+	person.show();
+	
+	
 	System.out.println("Main Ending.....");
 	
 	
